eaa25909f397f2522148ca06df0d7247f35d8619
dmlr7 <leviatandemon@gmail.com>
2014-03-29 12:59:08 -0500

    creacion de los controles faltantes

---
+++ src/java/co/edu/unal/arqdsoft/control/ControlSoporte.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.unal.arqdsoft.control;
+import co.edu.unal.arqdsoft.dao.*;
+
+/**
+ *
+ * @author dfoxpro
+ */
+public class ControlSoporte {
+    
+            
+}
