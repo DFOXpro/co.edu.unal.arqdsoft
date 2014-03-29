a95f76861ba6381c19e8a4119a8a3d7343f213e7
dfoxpro <dfoxpro@gmail.com>
2014-03-29 11:41:42 -0500

    metoto autentificar: busca al empleado por el nombre y comprueba que la contraseña sea la misma, de serlo retorna true

--- src/java/co/edu/unal/arqdsoft/control/Autenticacion.java
+++ src/java/co/edu/unal/arqdsoft/control/Autenticacion.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package co.edu.unal.arqdsoft.control;
+import co.edu.unal.arqdsoft.dao.*;
 
 /**
  *
@@ -13,14 +14,21 @@ public class Autenticacion {
 
     //TODO/**
     /**
-     *
-     * @param usuario
-     * @param contraseña
-     * @return esUsuario
+     * Comprueba que el usuario que esta ingresando 
+     * exista en la base de datos.
+     * 
+     * @param usuario usuario a comprobar
+     * @param contraseña la contraseña del usuario
+     * @return esUsuario devuelve true en caso de ser usario, false de no serlo
      */
-    public boolean cetificarUsuario(String usuario, String contraseña) {
+    public boolean CetificarUsuario(String usuario, String contraseña) {
         boolean esUsuario=false;
         
+        Empleado a=Empleado.Buscar(usuario);
+        if(contraseña.equals(a.contraseña)){
+            return true;
+        }
+                      
         return false;
     }
 }
