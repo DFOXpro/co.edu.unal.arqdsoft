0ff8a279ba987131849a7decca63cb95e68dd063
XPilo <jspoloa@unal.edu.co>
2014-03-29 13:02:21 -0500

    Actualizacion de las clases entidad. Se crearon las clases Cliente y Plan Clase Venta completa con sus atributos y metodos.

--- src/java/co/edu/unal/arqdsoft/entidad/Venta.java
+++ src/java/co/edu/unal/arqdsoft/entidad/Venta.java
@@ -6,10 +6,65 @@
 
 package co.edu.unal.arqdsoft.entidad;
 
+import java.util.Date;
+
 /**
  *
- * @author Jose
+ * @author jspoloa
  */
-class Venta {
+public class Venta {
+    private int id;
+    private Date fecha;
+    private Cliente cliente;
+    private Empleado vendedor;
+    private Plan plan;
+
+    public Venta(int id, Date fecha, Cliente cliente, Empleado vendedor, Plan plan) {
+        this.id = id;
+        this.fecha = fecha;
+        this.cliente = cliente;
+        this.vendedor = vendedor;
+        this.plan = plan;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Empleado getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(Empleado vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public Plan getPlan() {
+        return plan;
+    }
+
+    public void setPlan(Plan plan) {
+        this.plan = plan;
+    }
     
 }
