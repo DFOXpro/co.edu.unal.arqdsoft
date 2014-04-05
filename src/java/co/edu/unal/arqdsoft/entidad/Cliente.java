6f15150b5e8a2cf6760b2a0c20f434fe9f4119dc
XPilo <jspoloa@unal.edu.co>
2014-04-05 10:20:12 -0500

    Actualizacion de la clase Cliente Añadidas las anotaciones para manejar las relaciones OneToMany

--- src/java/co/edu/unal/arqdsoft/entidad/Cliente.java
+++ src/java/co/edu/unal/arqdsoft/entidad/Cliente.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 /**
