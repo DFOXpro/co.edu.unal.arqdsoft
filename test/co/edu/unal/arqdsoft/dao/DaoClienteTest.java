b049b573acc9f4c0e39238e68e1aa8f31892ccbb
XPilo <jspoloa@unal.edu.co>
2014-04-06 13:58:07 -0500

    Actualización  DaoCliente DaoCliente guarda un cliente, no permite el guardado de un cliente que tenga una id que ya este en la base de datos. Se creo DaoClienteTest para las pruebas.

---
+++ test/co/edu/unal/arqdsoft/dao/DaoClienteTest.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.unal.arqdsoft.dao;
+
+import co.edu.unal.arqdsoft.entidad.Cliente;
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jose
+ */
+public class DaoClienteTest {
+    
+    public DaoClienteTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of NuevoCliente method, of class DaoCliente.
+     */
+//    @Test
+//    public void testNuevoCliente_Cliente() {
+//        System.out.println("NuevoCliente");
+//        Cliente e = null;
+//        DaoCliente.NuevoCliente(e);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+
+    /**
+     * Test of NuevoCliente method, of class DaoCliente.
+     */
+    @Test
+    public void testNuevoCliente_3args() {
+        System.out.println("NuevoCliente");
+        int id = 80128928;
+        String nombre = "José Santiago Polo Acosta";
+        String informacion = "carrera 24 #48-50;3184654140";
+        boolean expResult = true;//true para la primera ejecucion, si no se cambia el id debe ponerse false
+        boolean result = DaoCliente.NuevoCliente(id, nombre, informacion);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getCliente method, of class DaoCliente.
+     */
+//    @Test
+//    public void testGetCliente() {
+//        System.out.println("getCliente");
+//        int idCliente = 0;
+//        DaoCliente instance = new DaoCliente();
+//        Cliente expResult = null;
+//        Cliente result = instance.getCliente(idCliente);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+
+    /**
+     * Test of getClientes method, of class DaoCliente.
+     */
+//    @Test
+//    public void testGetClientes() {
+//        System.out.println("getClientes");
+//        DaoCliente instance = new DaoCliente();
+//        ArrayList<Cliente> expResult = null;
+//        ArrayList<Cliente> result = instance.getClientes();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+    
+}
