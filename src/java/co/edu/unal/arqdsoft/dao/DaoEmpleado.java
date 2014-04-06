c9896dab9bd91552c694251c2b76da70b6506352
XPilo <jspoloa@unal.edu.co>
2014-04-06 17:08:29 -0500

    Actualización DaoEmpleado Se añadio el metodo buscarEmpleadosDisponibles() que devuelve un ArrayList con todos los empleados (tecnicos) que esten disponibles.

--- src/java/co/edu/unal/arqdsoft/dao/DaoEmpleado.java
+++ src/java/co/edu/unal/arqdsoft/dao/DaoEmpleado.java
@@ -3,15 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package co.edu.unal.arqdsoft.dao;
 
 import co.edu.unal.arqdsoft.entidad.Empleado;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NonUniqueResultException;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
@@ -20,7 +21,7 @@ import javax.persistence.Query;
  * @author dfoxpro
  */
 public class DaoEmpleado {
-    
+
     static EntityManagerFactory emf = Persistence.createEntityManagerFactory("co-edu-unal-arqdsoftPU");
 
     /**
@@ -35,29 +36,41 @@ public class DaoEmpleado {
      *
      * @return
      */
-    public static List<Empleado> buscarEmpleados() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static ArrayList<Empleado> buscarEmpleadosDisponibles() {
+        EntityManager em = emf.createEntityManager();
+        ArrayList<Empleado> empleados = new ArrayList<>();
+        Query q;
+
+        q = em.createQuery("SELECT e FROM Empleado e WHERE e.disponible=1");
+        em.getTransaction().begin();
+        try {
+            empleados = (ArrayList<Empleado>) q.getResultList();
+        } catch (Exception e) {
+            empleados = null;
+        } finally {
+            em.close();
+            return empleados;
+        }
     }
 
-    
     /**
-     * Este metodo solo se crea ara hacer pruebas de guardado en la base de datos
-     * ya que la administración de empleados se sale de nuestro alcance.
-     * @param empleado 
-     * @return  
+     * Este metodo solo se crea ara hacer pruebas de guardado en la base de
+     * datos ya que la administración de empleados se sale de nuestro alcance.
+     *
+     * @param empleado
+     * @return
      */
-    public boolean persist(Empleado empleado) {
+    public static boolean persist(Empleado empleado) {
         EntityManager em = emf.createEntityManager();
-        boolean exito=false;
+        boolean exito = false;
         try {
             em.getTransaction().begin();
             em.persist(empleado);
             em.getTransaction().commit();
-            exito=true;
+            exito = true;
         } catch (Exception e) {
-            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
             em.getTransaction().rollback();
-            exito=false;
+            exito = false;
         } finally {
             em.close();
             return exito;
@@ -65,34 +78,34 @@ public class DaoEmpleado {
     }
 
     /**
-     * Metodo encargado de hacer el login de usuario, si ocurre un error o los datos ingresados no
-     * corresponden a ningún usuario retorna null.
+     * Metodo encargado de hacer el login de usuario, si ocurre un error o los
+     * datos ingresados no corresponden a ningún usuario retorna null.
+     *
      * @param usuario
      * @param contrasena
-     * @return 
+     * @return
      */
-    
-    public static Empleado login(String usuario,String contrasena) {
+    public static Empleado login(String usuario, String contrasena) {
         EntityManager em = emf.createEntityManager();
-        Empleado empleado=null;
+        Empleado empleado = null;
         Query q;
-        q=em.createQuery("SELECT e FROM Empleado e WHERE e.usuario LIKE :usuario AND e.contrasena LIKE :contrasena")
-                .setParameter("usuario",usuario)
-                .setParameter("contrasena",contrasena);
-        
-        try{
+        q = em.createQuery("SELECT e FROM Empleado e WHERE e.usuario LIKE :usuario AND e.contrasena LIKE :contrasena")
+                .setParameter("usuario", usuario)
+                .setParameter("contrasena", contrasena);
+
+        try {
             em.getTransaction().begin();
-            empleado=(Empleado)q.getSingleResult();
-        }catch(Exception e){
+            empleado = (Empleado) q.getSingleResult();
+        } catch (Exception e) {
             //e.printStackTrace();
-            empleado=null;
+            empleado = null;
         }
-         em.close();
-         return empleado;
+        em.close();
+        return empleado;
     }
 
     public Empleado getEmpleado(int idOperador) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
