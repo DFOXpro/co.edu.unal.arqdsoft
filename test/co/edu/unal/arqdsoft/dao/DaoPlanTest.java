49116b180b003f6f63bdabcca16f3b1837ddec8e
Jhh <Jhh@jhh-2pc>
2014-04-06 20:29:00 -0500

    DaoPlan + DaoPlanTest

--- test/co/edu/unal/arqdsoft/dao/DaoPlanTest.java
+++ test/co/edu/unal/arqdsoft/dao/DaoPlanTest.java
@@ -84,7 +84,7 @@ public class DaoPlanTest {
         DaoPlan instance = new DaoPlan();
         
         Plan result = instance.getPlan(idPlan);
-        System.out.println(result);
+//      System.out.println(result);
         assertEquals(idPlan, result.getId());
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
