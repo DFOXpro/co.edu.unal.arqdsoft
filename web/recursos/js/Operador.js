40eb58d66e3969071d75839769759842f2f4ea76
dfoxpro <dfoxpro@gmail.com>
2014-04-08 18:50:26 -0500

    Empezando modulo operador

---
+++ web/recursos/js/Operador.js
@@ -0,0 +1,11 @@
+Operador = new Object();
+Operador.Soporte = new Object();
+Operador.Cliente = new Object();
+Operador.Soporte.get = function (){};
+Operador.Soporte.set = function (){};
+
+
+Operador.Cliente.get = function (){};
+
+Operador.innit = function (){};
+
