8dd217c4be5bd3f585156a32efe882a9facb81f3
XPilo <jspoloa@unal.edu.co>
2014-04-08 20:57:06 -0500

    actualizaciones menores

--- src/java/co/edu/unal/arqdsoft/dao/DaoCliente.java
+++ src/java/co/edu/unal/arqdsoft/dao/DaoCliente.java
@@ -19,13 +19,6 @@ import javax.persistence.Persistence;
 public class DaoCliente {
     static EntityManagerFactory emf = Persistence.createEntityManagerFactory("co-edu-unal-arqdsoftPU");
     
-    /**
-     *
-     * @param e
-     */
-    public static void NuevoCliente(Cliente e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
     /**
      *
