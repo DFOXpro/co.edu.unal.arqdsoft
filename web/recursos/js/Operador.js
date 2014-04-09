13840b876721377ff0550c7f99ba3e83378d2d5f
dfoxpro <dfoxpro@gmail.com>
2014-04-08 22:27:52 -0500

    Operador listo el JS y html calendario segun polo

--- web/recursos/js/Operador.js
+++ web/recursos/js/Operador.js
@@ -8,24 +8,30 @@ Operador.Soporte.set = function (){
     s = "";
     if ($("#op_idCliente").val().length < 6) s += "Escriba bien el numero de cedula.<br>";
     if ($("#op_nombreCliente").val() == "") s += "Qué cliente presenta el problema.<br>";
-    if ($("#op_planDano").find(":selected").val() == "-1") s += "Seleccione el plan dañado primero.<br>";
+    //if ($("#op_planDano").find(":selected").val() == "-1") s += "Seleccione el plan dañado primero.<br>";
     if ($("#op_informacionDano").val().length < 20) s += "Escriba una información detallada del error.<br>";
-    if ($("#op_solucionOfrecida").val().length < 20) s += "Escriba una solución dada detallada de la solucion dada.<br>";
-    if (
-        !($("#op_cb_EnvioDeTecnico").is(':checked') |
-        $("#op_cb_Solucinado").is(':checked'))
-    ) s += "Si no se solucionó el problema envía un tecnico.<br>";
+    //if ($("#op_solucionOfrecida").val().length < 20) s += "Escriba una solución dada detallada de la solucion dada.<br>";
+    if ($("#op_cb_Solucinado").is(':checked'))
+        if($("#op_cb_EnvioDeTecnico").is(':checked'))
+            s += "para qué enviar tecnico si se solucionó el problema.<br>";
+    else if(!$("#op_cb_EnvioDeTecnico").is(':checked'))
+        s += "Si no se solucionó el problema envía un tecnico.<br>";
+    if($("#op_cb_EnvioDeTecnico").is(':checked'))
+        if($("#op_i_fechaTecnico").val()=="" | new Date() > $("#op_i_fechaTecnico").datepicker("getDate"))
+            s += "Hay un error con la fecha.<br>";
 
     if(s != "") $("#error").html(s);
     else {
         var data = {
             cliente : $("#op_idCliente").val(),
-            plan: $("#op_planDano").find(":selected").val(),
+            //plan: $("#op_planDano").find(":selected").val(),
+            fechaTecnico: $("#op_i_fechaTecnico").datepicker("getDate").toJSON(),
             enviaTecnico: $("op_cb_EnvioDeTecnico").is(':checked'),
-            solucionado: $("op_cb_Solucinado").is(':checked'),
-            info: $("#op_informacionDano").val(),
-            solucion: $("#op_solucionOfrecida").val()
+            solucion: $("op_cb_Solucinado").is(':checked'),
+            info: $("#op_informacionDano").val()//,
+            //solucion: $("#op_solucionOfrecida").val()
         };
+//{"cliente":"1234567","enviaTecnico":false,"fechaTecnico":"2014-04-09T05:00:00.000Z","solucion":false,"info":"awqeqeqeqeqeqeqeqweqweqew"}
         sendRequest(
             "ventas",
             Operador.Soporte.respuesta,
@@ -77,5 +83,6 @@ Operador.innit = function (){
     });
     Evento.boton($("#op_b_enviarReporte"),Operador.Soporte.set);
     Evento.boton($("#op_b_resetReporte"),Operador.reset);
+    $("#op_i_fechaTecnico").datepicker({dateFormat: "DD dd 'de' MM"});
     Evento.cerrarSesion($("#l_op_cerrarSesion"));
 };
