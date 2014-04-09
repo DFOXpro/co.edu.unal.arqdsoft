2166bdc9a0e20068d00dd91a7503ae20db666cac
dfoxpro <dfoxpro@gmail.com>
2014-04-08 20:06:36 -0500

    Operador ya recive cliente

--- web/recursos/js/Operador.js
+++ web/recursos/js/Operador.js
@@ -5,7 +5,28 @@ Operador.Soporte.get = function (){};
 Operador.Soporte.set = function (){};
 
 
-Operador.Cliente.get = function (){};
+Operador.Cliente.mostrar = function (respuesta){
+    if(respuesta.error.length == 0){
+        $("#error").html("");
+        $("#op_idCliente").prop('disabled', true);
+        $("#op_idCliente").val(respuesta.contenido.dato.id);
+        $("#op_nombreCliente").val(respuesta.contenido.dato.nombre);
+    } else $("#error").html(respuesta.error);
+};
+Operador.Cliente.get = function (){
+    if($("#op_idCliente").val().length>5)
+        sendRequest(
+            "ventas",
+            Operador.Cliente.mostrar,
+            "getCliente",
+            $("#op_idCliente").val()
+        );
+    else $("#error").html("Escriba bien el numero de cedula");
+};
 
-Operador.innit = function (){};
-
+Operador.innit = function (){
+    $("#op_b_buscarCliente").submit(function(event) {
+        event.preventDefault();
+        Operador.Cliente.get();
+    });
+};
